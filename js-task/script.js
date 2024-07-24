ad625d7cd199e9e5dde75a65572e1430fd7deae9
sajan <sajankbastian@gmail.com>
2024-07-24 19:42:47 +0530

    task updated

--- js-task/script.js
+++ js-task/script.js
@@ -1,6 +1,6 @@
 //task-1
 
-{
+function reverseRightAnglePattern() {
     let str='';
 
     for(let i=1; i<=5; i++){
@@ -12,11 +12,12 @@
 
     console.log(str);
 }
+reverseRightAnglePattern();
 console.log('\n\n\n');
 
 //task-2
 
-{
+function numberPattern() {
     let str='';
 
     for(let i=1; i<=5; i++){
@@ -27,20 +28,22 @@ console.log('\n\n\n');
     }
     console.log(str);
 }
+numberPattern();
 console.log('\n\n\n');
 
 //task-3
 
-{
+function printOnetoTen () {
     for(let i=1; i<=10; i++){
         console.log(i);
     }
 }
+printOnetoTen ();
 console.log('\n\n\n');
 
 //task-4
 
-{
+function sumOfConsicutiveNumbers() {
     let sum=0;
 
     for(let i=1; i<=10; i++){
@@ -48,22 +51,43 @@ console.log('\n\n\n');
     }
     console.log(sum);
 }
+sumOfConsicutiveNumbers();
 console.log('\n\n\n');
 
 //task-5
 
-{
+function evenNumbers() {
     for(let i=1; i<=20; i++){
         if(i%2==0){
             console.log(i);
         }
     }
 }
+evenNumbers();
 console.log('\n\n\n');
 
 //task-6
 
-{
+function numberOfVowels() {
+    let name=['s','a','m','s','u','n'];
+    let v=['a','e','i','o','u'];
+    let count=0;
+
+    for(let i=0; i<=name.length-1; i++){
+        for(let j=0; j<=v.length-1; j++){
+            if(name[i]==v[j]){
+                count++;
+            }
+        }
+    }
+    console.log(count);
+}
+numberOfVowels();
+console.log('\n\n\n');
+
+//task-7
+
+function isPrime() {
     let count=0;
 
     let num=7;
@@ -79,6 +103,57 @@ console.log('\n\n\n');
         }else{
             console.log('not a prime number');
         }
-        
-    
 }
+isPrime();
+console.log('\n\n\n');
+
+//task-largest
+
+function LargestNumber() {
+    let arr=[10,7,21,19,30,8];
+    let largest=arr[0];
+
+    for(let i=0; i<=arr.length -1; i++){
+        if(arr[i]>largest){
+            largest=arr[i];
+        }
+
+    }
+    console.log("largest is : ",largest);
+}
+LargestNumber();
+console.log('\n\n\n');
+
+//task-smallest
+
+function smallestNumber() {
+    let arr=[10,7,21,19,30,8];
+    let smallest=arr[0];
+
+    for(let i=0; i<=arr.length -1; i++){
+        if(arr[i]<smallest){
+            smallest=arr[i];
+        }
+
+    }
+    console.log("smallest is : ",smallest);
+}
+smallestNumber();
+console.log('\n\n\n');
+
+//task-factorial
+
+function Factorial() {
+    let num=5;
+    let fact=1;
+    for(let i=1; i<=num; i++){
+        fact=fact*i;
+    }
+    console.log(fact);
+}
+Factorial();
+console.log('\n\n\n');
+
+
+
+
